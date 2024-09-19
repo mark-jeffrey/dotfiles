d6686191133bc5962664f05a2e7fc9afc23e58a9
Mark Jeffrey <markjeffrey@doctors.org.uk>
2024-09-19 01:19:26 +0100

    vi alias and .local/bin to PATH

--- nvim/.config/nvim/lua/config/autocmds.lua
+++ nvim/.config/nvim/lua/config/autocmds.lua
@@ -1,3 +1,8 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
--- Add any additional autocmds here
+-- Add any additional autocmds here 
+
+-- Add snazzy · and × for journaling
+vim.keymap.set('i', 'todo', '·')
+vim.keymap.set('i', 'done', '×')
+
--- zsh/.zshrc
+++ zsh/.zshrc
@@ -65,6 +65,7 @@ zstyle ':fzf-tab:complete:__zoxide_z:*' fzf-preview 'ls --color $realpath'
 # Aliases
 alias ls='lsd'
 alias vim='nvim'
+alias vi='nvim'
 alias c='clear'
 alias td='topydo'
 
