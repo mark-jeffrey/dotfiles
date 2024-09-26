f1b13351f54b5e4aac88ef9de681046deffc7eac
Mark Jeffrey <markjeffrey@doctors.org.uk>
2024-09-26 09:14:32 +0100

    waybar config and nvim journal keymaps, tmux rose pine theme

--- nvim/.config/nvim/lua/config/autocmds.lua
+++ nvim/.config/nvim/lua/config/autocmds.lua
@@ -2,7 +2,3 @@
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here 
 
--- Add snazzy · and × for journaling
-vim.keymap.set('i', 'todo', '·')
-vim.keymap.set('i', 'done', '×')
-
--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -1,6 +1,15 @@
-# Neotree
+-- Neotree
 vim.keymap.set("n", "<leader>t", "<Cmd>Neotree toggle<CR>", { desc = "Toggle Neotree" })
 vim.keymap.set("n", "<C-h>", "<Cmd>NvimTmuxNavigateLeft<CR>", { desc = "NvimTmux Navigate Left" })
 vim.keymap.set("n", "<C-j>", "<Cmd>NvimTmuxNavigateDown<CR>", { desc = "NvimTmux Navigate Down" })
 vim.keymap.set("n", "<C-k>", "<Cmd>NvimTmuxNavigateUp<CR>", { desc = "NvimTmux Navigate Up"})
 vim.keymap.set("n", "<C-l>", "<Cmd>NvimTmuxNavigateRight<CR>", { desc = "NvimTmux Navigate Right"})
+
+-- Bullet journal unicode signifiers
+require("which-key").add({ "<localleader>j", group = "Journal" })
+vim.keymap.set("n", "<localleader>jt", "i· ", { desc = "Todo" })
+vim.keymap.set("n", "<localleader>jr", "i△ ", { desc = "Reflection" })
+vim.keymap.set("n", "<localleader>jm", "i⊳ ", { desc = "Migrated" })
+vim.keymap.set("n", "<localleader>js", "i⊲ ", { desc = "Scheduled" })
+vim.keymap.set("n", "<localleader>jd", "0cl⨯<Esc>", { desc = "Mark todo as done" })
+
