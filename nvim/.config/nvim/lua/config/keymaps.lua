f8c7bac415461237c5f7d507214c23fed1bc8811
Mark Jeffrey <markjeffrey@doctors.org.uk>
2025-04-22 07:14:20 +0100

    neovim zk plugin config

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,6 @@
+-- Keymaps and which-key mappings by plugin
+local wk = require("which-key")
+
 -- Neotree
 vim.keymap.set("n", "<leader>t", "<Cmd>Neotree toggle<CR>", { desc = "Toggle Neotree" })
 vim.keymap.set("n", "<C-h>", "<Cmd>NvimTmuxNavigateLeft<CR>", { desc = "NvimTmux Navigate Left" })
@@ -5,13 +8,68 @@ vim.keymap.set("n", "<C-j>", "<Cmd>NvimTmuxNavigateDown<CR>", { desc = "NvimTmux
 vim.keymap.set("n", "<C-k>", "<Cmd>NvimTmuxNavigateUp<CR>", { desc = "NvimTmux Navigate Up" })
 vim.keymap.set("n", "<C-l>", "<Cmd>NvimTmuxNavigateRight<CR>", { desc = "NvimTmux Navigate Right" })
 
--- Bullet journal unicode signifiers
-require("which-key").add({ "<localleader>j", group = "Journal" })
-vim.keymap.set("n", "<localleader>jt", "i· ", { desc = "Todo" })
-vim.keymap.set("n", "<localleader>jr", "i△ ", { desc = "Reflection" })
-vim.keymap.set("n", "<localleader>jm", "i⊳ ", { desc = "Migrated" })
-vim.keymap.set("n", "<localleader>js", "i⊲ ", { desc = "Scheduled" })
-vim.keymap.set("n", "<localleader>jd", "0cl⨯<Esc>", { desc = "Mark todo as done" })
-
 -- Zen Mode
-vim.keymap.set("n", "<leader>z", ":ZenMode<cr>", { desc = "Zen Mode" })
+vim.keymap.set("n", "<leader>zz", ":ZenMode<cr>", { desc = "Zen Mode" })
+wk.add({ "<leader>zz", icon = { icon = " ", color = "purple" } })
+
+-- zk
+wk.add({ "<leader>z", group = "zk notes", icon = { icon = "󱞁 ", color = "orange" } })
+vim.keymap.set("n", "<leader>zn", "<Cmd>ZkNew { title = vim.fn.input('Title: ') }<CR>", { desc = "Create new zk note" })
+vim.keymap.set("n", "<leader>zo", "<Cmd>ZkNotes { sort = { 'modified' } }<CR>", { desc = "Open notes" })
+vim.keymap.set("n", "<leader>zt", "<Cmd>ZkTags<CR>", { desc = "Open notes associated with selected tags" })
+vim.keymap.set(
+  "n",
+  "<leader>zf",
+  "<Cmd>ZkNotes { sort = { 'modified' }, match = { vim.fn.input('Search: ') } }<CR>",
+  { desc = "Search for notes matching a given query" }
+)
+vim.keymap.set("v", "<leader>zf", ":'<,'>ZkMatch<CR>", { desc = "Search for notes matching current visual selection" })
+
+-- zk: only for Markdown files in a zk notebook.
+if vim.bo.filetype == "markdown" then
+  if require("zk.util").notebook_root(vim.fn.expand("%:p")) ~= nil then
+    vim.keymap.set(
+      "n",
+      "<CR>",
+      "<Cmd>lua vim.lsp.buf.definition()<CR>",
+      { buffer = true, desc = "Open link under the caret" }
+    )
+    vim.keymap.set(
+      "n",
+      "<leader>zn",
+      "<Cmd>ZkNew { dir = vim.fn.expand('%:p:h'), title = vim.fn.input('Title: ') }<CR>",
+      { buffer = true, desc = "Create zk note in same directory as current buffer" }
+    )
+    vim.keymap.set(
+      "v",
+      "<leader>znt",
+      ":'<,'>ZkNewFromTitleSelection { dir = vim.fn.expand('%:p:h') }<CR>",
+      { buffer = true, desc = "Create note in same directory as current buffer using selection as title" }
+    )
+    vim.keymap.set(
+      "v",
+      "<leader>znc",
+      ":'<,'>ZkNewFromContentSelection { dir = vim.fn.expand('%:p:h'), title = vim.fn.input('Title: ') }<CR>",
+      { buffer = true, desc = "Create note in same directory as current buffer, using selection as note content" }
+    )
+    vim.keymap.set(
+      "n",
+      "<leader>zb",
+      "<Cmd>ZkBacklinks<CR>",
+      { buffer = true, desc = "Open notes linking to current buffer" }
+    )
+    vim.keymap.set(
+      "n",
+      "<leader>zl",
+      "<Cmd>ZkLinks<CR>",
+      { buffer = true, desc = "Open notes linked by the current buffer" }
+    )
+    vim.keymap.set("n", "K", "<Cmd>lua vim.lsp.buf.hover()<CR>", { buffer = true, desc = "Preview a linked note" })
+    vim.keymap.set(
+      "v",
+      "<leader>za",
+      ":'<,'>lua vim.lsp.buf.range_code_action()<CR>",
+      { buffer = true, desc = "Open code actions for visual selection" }
+    )
+  end
+end
